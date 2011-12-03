14e3907ef8ded062fb48b18b4cb4a1c7b09ab6f7
cesine <cesine@host>
2011-12-03 14:58:16 -0500

    added preferences to keep static data

---
+++ android/src/ca/ilanguage/rhok/imageupload/pref/PreferenceConstants.java
@@ -0,0 +1,13 @@
+package ca.ilanguage.rhok.imageupload.pref;
+
+public class PreferenceConstants {
+	
+	public static final String PREFERENCE_NAME = "ImageUploadPrefs";
+	public static final String PREFERENCE_WATER_SAMPLE_ID = "watersampleId";
+    
+    public static final String PREFERENCE_TESTING_DAY_NUMBER = "testingdaynumber";
+     
+    public static final String PREFERENCE_WATER_SAMPLE_STARTTIME = "samplestarttime";
+    public static final String PREFERENCE_WATER_SAMPLE_ENDTIME = "sampleendtime";
+   
+}
