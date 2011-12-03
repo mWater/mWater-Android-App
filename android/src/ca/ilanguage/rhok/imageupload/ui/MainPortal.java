0102437ce6f940369576a7051051995ad7228ea7
George Peristerakis <george@lesite.ca>
2011-12-03 17:29:40 -0500

    Added event handlers to the main portal

--- android/src/ca/ilanguage/rhok/imageupload/ui/MainPortal.java
+++ android/src/ca/ilanguage/rhok/imageupload/ui/MainPortal.java
@@ -1,8 +1,11 @@
 package ca.ilanguage.rhok.imageupload.ui;
 
 import ca.ilanguage.rhok.imageupload.R;
+import ca.ilanguage.rhok.imageupload.service.TakePicture;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPortal extends Activity {
 
@@ -11,4 +14,21 @@ public class MainPortal extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
+
+	public void onWaterSourceClick(View v) {
+		Intent intent = new Intent(this, TakePicture.class);
+
+		startActivity(intent);
+	}
+
+	public void onWaterResultsClick(View v) {
+		Intent intent = new Intent(this, TakePicture.class);
+
+		startActivity(intent);
+	}
+
+	public void onSyncServerClick(View v) {
+		startActivity(new Intent(this, ServerSync.class));
+	}
+
 }
