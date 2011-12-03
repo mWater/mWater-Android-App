73905e1cd72c47dcb9c8b7c6721bffb504c0df07
George Peristerakis <george@lesite.ca>
2011-12-03 16:36:22 -0500

    Added the icons for the main portal view

--- android/src/ca/ilanguage/rhok/imageupload/ui/MainPortal.java
+++ android/src/ca/ilanguage/rhok/imageupload/ui/MainPortal.java
@@ -1,7 +1,14 @@
 package ca.ilanguage.rhok.imageupload.ui;
 
+import ca.ilanguage.rhok.imageupload.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainPortal extends Activity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
