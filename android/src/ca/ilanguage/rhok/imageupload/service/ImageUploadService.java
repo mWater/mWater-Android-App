76f30c46b68865fcc06a613f02844c6965fc9cd5
George Peristerakis <george@lesite.ca>
2011-12-03 15:24:01 -0500

    simple import fixes

--- android/src/ca/ilanguage/rhok/imageupload/service/ImageUploadService.java
+++ android/src/ca/ilanguage/rhok/imageupload/service/ImageUploadService.java
@@ -1,11 +1,17 @@
 package ca.ilanguage.rhok.imageupload.service;
 
+import ca.ilanguage.rhok.imageupload.R;
 import ca.ilanguage.rhok.imageupload.pref.PreferenceConstants;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 
 public class ImageUploadService extends IntentService {
 	protected static String TAG = "NotifyingTranscriptionIntentService";
@@ -17,7 +23,7 @@ public class ImageUploadService extends IntentService {
 	private PendingIntent mContentIntent;
 	private int mAuBlogIconId = R.drawable.stat_aublog;
 
-	public NotifyingTranscriptionIntentService() {
+	public ImageUploadService() {
 		super(TAG);
 	}
 
