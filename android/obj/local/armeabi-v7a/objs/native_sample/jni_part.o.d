8f2927b0cffdec1d431c41869e06b8ec90530d2d
cesine <cesine@host>
2011-12-11 12:15:59 -0500

    incomplete refactoring to NDK android project
    Your eclipse setup will be more complex from here on.
    Added:
    jni
    libs
    obj
    
    The workspace is not compiling yet, here is the output
    jni/Android.mk:5: ../includeOpenCV.mk: No such file or directory
    jni/Android.mk:8: /Users/gina/android/android-ndk-r7/toolchains/arm-linux-androideabi-4.4.3/prebuilt/darwin-x86/user/share/OpenCV/OpenCV.mk: No such file or directory
    make: *** No rule to make target `/Users/gina/android/android-ndk-r7/toolchains/arm-linux-androideabi-4.4.3/prebuilt/darwin-x86/user/share/OpenCV/OpenCV.mk'.  Stop.

---
+++ android/obj/local/armeabi-v7a/objs/native_sample/jni_part.o.d
@@ -0,0 +1,142 @@
+obj/local/armeabi-v7a/objs/native_sample/jni_part.o: jni/jni_part.cpp \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/jni.h \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/core.hpp \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/types_c.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/version.hpp \
+ /Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/algorithm \
+ /Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/cmath \
+ /Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/complex \
+ /Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/map \
+ /Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/vector \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/operations.hpp \
+ /Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/limits \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/mat.hpp \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/imgproc/imgproc.hpp \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/imgproc/types_c.h \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/core_c.h \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/features2d/features2d.hpp \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/flann/miniflann.hpp \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/flann/defines.h \
+ ../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/flann/config.h
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/jni.h:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/core.hpp:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/types_c.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/gina/android/android-ndk-r7/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/version.hpp:
+
+/Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/algorithm:
+
+/Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/cmath:
+
+/Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/complex:
+
+/Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/map:
+
+/Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/vector:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/operations.hpp:
+
+/Users/gina/android/android-ndk-r7/sources/cxx-stl/gnu-libstdc++/include/limits:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/mat.hpp:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/imgproc/imgproc.hpp:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/imgproc/types_c.h:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/core/core_c.h:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/features2d/features2d.hpp:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/flann/miniflann.hpp:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/flann/defines.h:
+
+../../OpenCV-2.3.1/share/OpenCV/../../include/opencv2/flann/config.h:
