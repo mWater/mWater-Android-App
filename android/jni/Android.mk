2c4bebb40e535440f31f16b270472fe1eeeeaeaf
Clayton <clayton@claytronics.org>
2012-05-31 13:57:09 -0400

    Switched to 2.4.0 of OpenCV and adjusted paths.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #OPENCV_PACKAGE_DIR should be set in your Builder, and point to something like this on your system:
-# /Users/<somename>/opencv/OpenCV-2.3.1-android-bin
+# /Users/<somename>/opencv/OpenCV-2.4.0-android-bin
 
-OPENCV_MK_PATH:=$(OPENCV_PACKAGE_DIR)/OpenCV-2.3.1/share/OpenCV/OpenCV.mk
+OPENCV_MK_PATH:=$(OPENCV_PACKAGE_DIR)/share/OpenCV/OpenCV.mk
 include $(OPENCV_MK_PATH)
 
 LOCAL_MODULE    := native_sample
